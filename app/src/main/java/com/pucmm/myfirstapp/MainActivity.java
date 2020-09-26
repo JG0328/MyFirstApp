a26312220db819a58e5487cef219f70253ae39ae
José Gabriel González <jgg032899@gmail.com>
2020-09-26 19:02:08 -0400

    Setting up DatePicker

--- app/src/main/java/com/pucmm/myfirstapp/MainActivity.java
+++ app/src/main/java/com/pucmm/myfirstapp/MainActivity.java
@@ -3,12 +3,26 @@ package com.pucmm.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText txtName, txtLastName;
+    private RadioGroup rdgLikes;
+    private Spinner spnGender;
+    private CheckBox chkJava, chkPython, chkJavaScript, chkGo, chkC, chkCSharp;
+    private Button btnClean, btnSend;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtName = findViewById(R.id.name_txt);
+        txtLastName = findViewById(R.id.last_name_txt);
     }
 }
